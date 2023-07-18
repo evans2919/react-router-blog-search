04a728dbb3b7f46675af34816c600f09eedde089
evans2919 <evansalcantara29@outlook.com>
2023-07-18 22:12:00 +0200

    Eliminación de uso useSearchParams

--- src/pages/single/BlogSingle.jsx
+++ src/pages/single/BlogSingle.jsx
@@ -1,4 +1,4 @@
-import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 const BlogSingle = () => {
     const params = useParams();
